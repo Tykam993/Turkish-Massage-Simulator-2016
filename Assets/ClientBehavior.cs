3b051148d60d43f5c885546aa94953dc69804205
David Trudeau <trudeaudm@gmail.com>
2015-11-23 11:46:09 -0500

    Random Pain generation, added client behavior.

---
+++ Assets/ClientBehavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientBehavior : MonoBehaviour {
+
+    public int painAmt;
+    public string clientName;
+    public GameObject painInd;
+    public int painCount = 0;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (painCount <= painAmt)
+        {
+
+            CastPain();
+        }
+
+
+	}
+    void CastPain ()
+    {
+        Debug.Log("paincast");
+        float randX = Random.Range(-0.5f, 0.5f);
+        float randZ = Random.Range(-0.5f, 1f);
+        RaycastHit hit;
+
+        Vector3 castFrom = transform.position;
+        castFrom.y = castFrom.y + 2;
+        castFrom.x = castFrom.x + randX;
+        castFrom.z = castFrom.z + randZ;
+        if (Physics.Raycast(castFrom, transform.forward, out hit, 10.0f))
+        {
+            if (hit.transform.tag == "Client")
+            {
+                GameObject newPain = (GameObject)Instantiate(painInd, hit.point, transform.rotation);
+                newPain.transform.parent = hit.transform;
+                painCount = painCount + 1;
+            }
+        }
+
+    }
+}
