4fb6e5dd1d099635aad6d9e5167113178cc4e75f
David Trudeau <trudeaudm@gmail.com>
2015-11-29 22:38:03 -0500

    Ragdolled, Cactus. Added to clients, created a new function for spawning specific clients which can be used by the UI. Simply assign the function to a button. It is within the management script. Called "SpawnClient()" SpawnClientRand() will be used until UI is functioning.

--- Assets/Scripts/Management.cs
+++ Assets/Scripts/Management.cs
@@ -7,9 +7,13 @@ public class Management : MonoBehaviour {
 
     public GameObject client1;
     public GameObject client2;
+    public GameObject client3;
     private GameObject currentClient;
     
-    private bool skel = false;
+
+
+    public int time;
+    private int clock;
 
     public Text stamVal;
     public Text cumfVal;
@@ -17,7 +21,7 @@ public class Management : MonoBehaviour {
     public AudioClip success;
     // Use this for initialization
     void Start () {
-        skel = false;
+
 	}
 	
 	// Update is called once per frame
@@ -28,37 +32,119 @@ public class Management : MonoBehaviour {
             Win();
         }
 	}
-    public void SpawnClient()
+    public void SpawnClient(GameObject client)
     {
 
-        if (skel == false)
+        if (client)
         {
             if (currentClient)
             {
                 currentClient.GetComponent<InteractiveObject>().RemoveMe();
             }
-            GameObject newClient = (GameObject)Instantiate(client2, transform.position, transform.rotation);
+            GameObject newClient = (GameObject)Instantiate(client, transform.position, transform.rotation);
 
             currentClient = newClient;
             float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
             cumfVal.text = startCumf.ToString();
             curCliVal = startCumf;
-            skel = true;
         }
-        else if (skel == true)
+        else // if the UI has not given the manager a client to spawn then the manager will pick one randomly.
         {
-            if (currentClient)
+            float rNum = Random.Range(1, 3);
+            if (rNum <= 1)
             {
-                currentClient.GetComponent<InteractiveObject>().RemoveMe();
-            }
-            GameObject newClient = (GameObject)Instantiate(client1, transform.position, transform.rotation);
+                if (currentClient)
+                {
+                    currentClient.GetComponent<InteractiveObject>().RemoveMe();
+                }
+                GameObject newClient = (GameObject)Instantiate(client1, transform.position, transform.rotation);
 
-            
-            currentClient = newClient;
-            float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
-            cumfVal.text = startCumf.ToString();
-            skel = false;
+                currentClient = newClient;
+                float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
+                cumfVal.text = startCumf.ToString();
+                curCliVal = startCumf;
+
+            }
+            else if (rNum >1 && rNum <=2)
+            {
+                if (currentClient)
+                {
+                    currentClient.GetComponent<InteractiveObject>().RemoveMe();
+                }
+                GameObject newClient = (GameObject)Instantiate(client2, transform.position, transform.rotation);
+
+
+                currentClient = newClient;
+                float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
+                cumfVal.text = startCumf.ToString();
+
+            }
+            else if (rNum > 2 && rNum <=3)
+            {
+                if (currentClient)
+                {
+                    currentClient.GetComponent<InteractiveObject>().RemoveMe();
+                }
+                GameObject newClient = (GameObject)Instantiate(client3, transform.position, transform.rotation);
+
+
+                currentClient = newClient;
+                float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
+                cumfVal.text = startCumf.ToString();
+
+            }
         }
+
+
+
+    }
+    public void SpawnClientRand()
+    {
+
+            float rNum = Random.Range(1, 4);
+            if (rNum <= 1)
+            {
+                if (currentClient)
+                {
+                    currentClient.GetComponent<InteractiveObject>().RemoveMe();
+                }
+                GameObject newClient = (GameObject)Instantiate(client1, transform.position, transform.rotation);
+
+                currentClient = newClient;
+                float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
+                cumfVal.text = startCumf.ToString();
+                curCliVal = startCumf;
+
+            }
+            else if (rNum > 1 && rNum <= 2)
+            {
+                if (currentClient)
+                {
+                    currentClient.GetComponent<InteractiveObject>().RemoveMe();
+                }
+                GameObject newClient = (GameObject)Instantiate(client2, transform.position, transform.rotation);
+
+
+                currentClient = newClient;
+                float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
+                cumfVal.text = startCumf.ToString();
+
+            }
+            else if (rNum > 2 && rNum <= 3)
+            {
+                if (currentClient)
+                {
+                    currentClient.GetComponent<InteractiveObject>().RemoveMe();
+                }
+                GameObject newClient = (GameObject)Instantiate(client3, transform.position, transform.rotation);
+
+
+                currentClient = newClient;
+                float startCumf = 100 - currentClient.GetComponent<InteractiveObject>().totalPain;
+                cumfVal.text = startCumf.ToString();
+
+            }
+        
 
 
 
